30b0c7b0bde7598ac29c760c6ea36f5f216dd9d5
huan.yang.sh <huan.yang.sh@dianping.com>
2017-07-27 23:15:59 +0800

    .

---
+++ 2-render/index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by yanghuan on 17/7/15.
+ */
+
+import React from 'react';
+import { render, } from 'react-dom';
+
+// 更新UI的唯一方法是创建一个新的元素
+// React只更新必需要更新的部分
+
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello,world1</h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+
+    render(element, document.getElementById('app'));
+}
+
+setInterval(tick, 1);
