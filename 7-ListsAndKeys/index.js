eee6f993aa3a79a0ff7e71e7f6f7a060e16e3175
huan.yang.sh <huan.yang.sh@dianping.com>
2017-07-30 22:39:28 +0800

    .

---
+++ 7-ListsAndKeys/index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by yanghuan on 17/7/15.
+ */
+
+import React from 'react';
+import { render, } from 'react-dom';
+
+
+// 元素中调用 map() 需要 keys，keys在同辈元素中必须是唯一的；
+
+function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        <li key={number.toString()}>
+            {number}
+        </li>
+    );
+
+    return (
+        <ul>{listItems}</ul> /*展开数组的每一项*/
+    );
+}
+
+const numbers = [1, 2, 3, 4, 5];
+render(
+    <NumberList numbers={numbers} />,
+    document.getElementById('app')
+);
+
+
