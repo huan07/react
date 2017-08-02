07d30931d1a0fae19ea02a33d6c69b663fdc555d
huan.yang.sh <huan.yang.sh@dianping.com>
2017-08-02 22:47:38 +0800

    .

---
+++ 10-Composition-vs-Inheritance/index.js
@@ -0,0 +1,104 @@
+/**
+ * Created by yanghuan on 17/7/15.
+ */
+
+import React from 'react';
+import { render, } from 'react-dom';
+
+// 同一个数据的变化需要几个不同的组件来反映。建议提升 共享的状态 到它们最近的祖先组件中（状态提升）
+
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) { /*  传函数 ＝＝＝＝＝＝＝*/
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water would boil</p>
+    }
+    return <p>The water would not boil</p>
+}
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.props.onTemperatureChange(event.target.value);
+        /*  ＝＝＝＝＝＝＝*/
+    }
+
+    render() {
+        const temperature = this.props.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={temperature}
+                       onChange={this.handleChange}
+                />
+            </fieldset>
+        )
+    }
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { temperature: '', scale: 'c', };
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    }
+
+    handleCelsiusChange(temperature) {
+        this.setState({ scale: 'c', temperature, });
+        /*＝＝＝＝＝＝＝＝＝＝＝＝*/
+    }
+
+    handleFahrenheitChange(temperature) {
+        this.setState({ scale: 'f', temperature, });
+        /*＝＝＝＝＝＝＝＝＝＝＝＝＝*/
+    }
+
+    render() {
+        const { temperature, scale, } = this.state;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+        return (
+            <div>
+                <TemperatureInput scale="c" temperature={celsius}
+                                  onTemperatureChange={this.handleCelsiusChange} />
+                <TemperatureInput scale="f" temperature={fahrenheit}
+                                  onTemperatureChange={this.handleFahrenheitChange} />
+                <BoilingVerdict celsius={parseFloat(celsius)} />
+            </div>
+        );
+    }
+}
+
+render(
+    <Calculator />,
+    document.getElementById('app')
+);
