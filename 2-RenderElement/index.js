8e33720549689f3159ced35daa85530775a3c205
huan07 <845265003@qq.com>
2017-09-12 05:17:49 +0800

    .

--- 2-RenderElement/index.js
+++ 2-RenderElement/index.js
@@ -12,7 +12,7 @@ function tick() {
     const element = (
         <div>
             <h1>Hello,world1</h1>
-            <h2>It is {new Date().toLocaleTimeString()}</h2>
+            <h2>It is {`${new Date().toLocaleDateString()}=====${new Date().toLocaleTimeString()}`}</h2>
         </div>
     );
 
