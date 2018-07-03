138c3ad059e1b62ee42312835bcd3588da25f69d
huan07 <845265003@qq.com>
2018-07-03 16:26:19 +0800

    .

--- 7-ListsAndKeys.js
+++ 7-ListsAndKeys.js
@@ -7,11 +7,14 @@ import { render, } from 'react-dom';
 
 
 // 调用 map() 或者 数组的元素是dom 才需要 keys，keys在同辈元素中必须是唯一的；
-// key的值为字符串
-// 键是React的一个内部映射，但其不会传递给组件的内部
+
+// key的值为字符串，帮助 React 标识哪个项被修改、添加或者移除
+
 // 如果列表项可能被重新排序时，我们不建议使用索引作为 keys，因为这导致一定的性能问题，会很慢
 
-function NumberList(props) {
+// 键是React的一个内部映射，但其不会传递给组件的内部
+
+function NumberList(props){
     const numbers = props.numbers;
     const listItems = numbers.map((number) =>
         <li key={number.toString()}>
@@ -20,7 +23,7 @@ function NumberList(props) {
     );
 
     return (
-        <ul>{listItems}</ul> /*展开数组的每一项*/
+        <ul>{listItems}</ul> // 展开数组的每一项
     );
 }
 
