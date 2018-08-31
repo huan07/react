8b165693839c819c170fe32edb048ae2184499d9
huan07 <845265003@qq.com>
2018-08-31 13:24:20 +0800

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,37 @@
-module.exports = {
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+var config = {
+    mode: "development",
     entry: {
-        //bundle1: './1-JSX.js',
-        //bundle2: './2-RenderingElements.js',
-        //bundle3: './3-ComponentsAndProps.js',
-
-        //bundle4: './4-StateAndLifecycle.js',
-        //bundle4_2:'./4_2-faq-ajax.js',
-        bundle4_3: './4_3-faq-state.js',
-        //bundle4_4: './4_4-lifecycle-mount.js',
-        //bundle4_5: './4_5-lifecycle-update.js',
-
-
-        //bundle5: './5-HandlingEvents.js',
-        //bundle5_2: './5_2-faq-functions.js',
-        //bundle5_3: './5_3-faq-functions.js',
-        //bundle5_I:'./5_Issues.js',
-
-
-        //bundle6: './6-ConditionalRendering.js',
-        //bundle7: './7-ListsAndKeys.js',
-
-        //bundle8: './8-ControlledComponents.js',
-        //bundle8_2: './8_2-UncontrolledComponents.js',
-        //bundle8_3: './8_3-RefsAndTheDom.js',
-        //bundle8_4: './8_4-ForwardingRefs.js',
-
-        //bundle9: './9-LiftingStateUp.js',
-        //bundle10: './10-CompositionVsInheritance.js'
-
-
-        //Context: './Context.js',
-        //Context2: './Context2.js',
-        //ErrorBoundaries: './ErrorBoundaries.js',
-        //RenderProps:'./RenderProps.js',
-        //bundle16: './16-Fragments',
-        //HigherOrderComponents: './HigherOrderComponents.js',
-        //bundle18: './18-Integrating',
-        //bundle19: './19-JSXInDepth.js',
-        //bundle20: './20-OptimizingPerformance',
-
-
-        //Reconciliation: './Reconciliation.js',
-
-
+        JSX: './src/JSX.js',
+        RenderingElements: './src/RenderingElements',
     },
     output: {
-        filename: '[name].js',
-        publicPath: ''
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
         ]
-    }
-}
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: __dirname + "/index.html"
+        })
+    ]
+};
+
+module.exports = config;
